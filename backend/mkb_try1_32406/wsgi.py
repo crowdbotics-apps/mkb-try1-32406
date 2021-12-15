04b2897c43fd0c8f5b9db698c4e2bd2a7118f908
Crowdbotics <team@crowdbotics.com>
2021-12-15 16:05:47 +0000

    Initial commit.

---
+++ backend/mkb_try1_32406/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mkb_try1_32406 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkb_try1_32406.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mkb_try1_32406",
+  url: "https://mkb_try1_32406.botics.co",
+  api: "https://mkb_try1_32406.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
